1c1ad583561dbae6f777e4f10a62d1741d77c134
Rafa041 <91387599+Rafa041@users.noreply.github.com>
2025-06-28 02:15:48 +0100

    Add ApplicationContext :D

---
+++ MechaSoft.Data/Context/ApplicationDbContext.cs
@@ -0,0 +1,51 @@
+﻿using MechaSoft.Data.Configuration;
+using MechaSoft.Domain.Model;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MechaSoft.Data.Context;
+
+public class ApplicationDbContext : DbContext
+{
+    public DbSet<Customer> Customers { get; set; }
+    public DbSet<Vehicle> Vehicles { get; set; }
+    public DbSet<Employee> Employees { get; set; }
+    public DbSet<Service> Services { get; set; }
+    public DbSet<Part> Parts { get; set; }
+    public DbSet<ServiceOrder> ServiceOrders { get; set; }
+    public DbSet<ServiceItem> ServiceItems { get; set; }
+    public DbSet<PartItem> PartItems { get; set; }
+    public DbSet<Inspection> Inspections { get; set; }
+
+    public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+        : base(options)
+    {
+    }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        base.OnConfiguring(optionsBuilder);
+    }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.ApplyConfigurationsFromAssembly(System.Reflection.Assembly.GetExecutingAssembly());
+        base.OnModelCreating(modelBuilder);
+
+        modelBuilder.ApplyConfiguration(new CustomerConfiguration());
+        modelBuilder.ApplyConfiguration(new VehicleConfiguration());
+        modelBuilder.ApplyConfiguration(new EmployeeConfiguration());
+        modelBuilder.ApplyConfiguration(new ServiceConfiguration());
+        modelBuilder.ApplyConfiguration(new PartConfiguration());
+        modelBuilder.ApplyConfiguration(new ServiceOrderConfiguration());
+        modelBuilder.ApplyConfiguration(new ServiceItemConfiguration());
+        modelBuilder.ApplyConfiguration(new PartItemConfiguration());
+        modelBuilder.ApplyConfiguration(new InspectionConfiguration());
+
+        modelBuilder.HasDefaultSchema("MechaSoft");
+    }
+}

7601f4451858bd99034973806a4b4b3898e372f2
rafae <rafae@DESKTOP-5POQFF0>
2025-06-30 23:46:51 +0100

    Context and create Customer, Employee, Repository

--- MechaSoft.Data/Repositories/Repository.cs
+++ MechaSoft.Data/Repositories/Repository.cs
@@ -1,7 +1,6 @@
 ﻿using MechaSoft.Data.Context;
 using MechaSoft.Domain.Core.Interfaces;
 using Microsoft.EntityFrameworkCore;
-using System.Data.Entity;
 using System.Linq;
 using System.Linq.Expressions;
 
